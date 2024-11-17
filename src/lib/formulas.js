4a6e7715d831815e1a8081f9a231a9f279843d83
Shawn Quereshi <saqneo@gmail.com>
2024-11-17 09:51:41 -0500

    Fix money reading for formula API usage check.

--- src/lib/formulas.js
+++ src/lib/formulas.js
@@ -5,5 +5,5 @@ export async function main(ns) {
 
 /** @param {import('../..').NS} ns */
 export function shouldUseFormulas(ns) {
-    return ns.fileExists('Formulas.exe') && ns.getServer('home').moneyAvailable > 2**32
+    return ns.fileExists('Formulas.exe') && ns.getPlayer().money > 2**32
 }
