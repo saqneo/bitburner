1fc4178393d73e7206eca30be694b9abccb0b17a
Shawn Quereshi <saqneo@gmail.com>
2025-11-27 12:37:49 -0500

    feat: Streamline killall.js for direct execution
    - Removed confirmation prompt for immediate execution.
    - Removed 'Done.' message as the operation cannot fail in-game.

--- src/util/killall.js
+++ src/util/killall.js
@@ -2,12 +2,6 @@ import { killAll } from '/lib/process.js';
 
 /** @param {NS} ns */
 export async function main(ns) {
-    const confirm = await ns.prompt("Are you sure you want to KILL ALL scripts on ALL servers (except this one)?");
-    if (confirm) {
-        ns.tprint("Killing all scripts...");
-        killAll(ns);
-        ns.tprint("Done.");
-    } else {
-        ns.tprint("Operation cancelled.");
-    }
+    ns.tprint("Killing all scripts...");
+    killAll(ns);
 }
